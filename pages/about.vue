c80eff52874a69782fb000a18d9c4c64e268c198
Tudor <tudor@seenons.com>
2021-11-21 17:21:51 +0100

    add dynamic titles

--- pages/about.vue
+++ pages/about.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="class">
-
-      <h1>Learning Rebasig and amending commits</h1>
+    <ul>
+      <h1>{{ title }}</h1>
+      <li>What a ride</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  head(){
-    return{
-      title: 'About dad JOkes',
-      meta:[
+  data() {
+    return {
+      title: "Learning Rebasig and amending commits",
+    };
+  },
+  head() {
+    return {
+      title: "About dad JOkes",
+      meta: [
         {
-          hid: 'description',
-          name:'description',
-          content:'best place for dad jokes'
-        }
-      ]
-
-    }
-  }
-
-}
+          hid: "description",
+          name: "description",
+          content: "best place for dad jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
